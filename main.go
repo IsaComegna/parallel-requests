a62b2cd72618c48f5977e2a13c7a68647179765b
Isabela Comegna <isacomegna@gmail.com>
2021-08-28 15:10:55 -0300

    Execute program concurrently

--- main.go
+++ main.go
@@ -5,34 +5,92 @@ import (
 	"fmt"
 	_ "net/http/pprof"
 	"os"
+	"sync"
 
 	"github.com/IsaComegna/parallel-requests/core/usecases"
 	gateways "github.com/IsaComegna/parallel-requests/gateways/http"
 )
 
-var concurrencyDefaultValue = 0
+var concurrencyDefaultValue = 10
+var concurrencyFlagNotPresent = 0
+var concurrencyMaxValue = 50
 
 func main() {
-	concurrencyPtr := flag.Int("parallel", concurrencyDefaultValue, "concurrency")
+	var (
+		concurrency int
+		URLs        []string
+	)
+
+	concurrencyPtr := flag.Int("parallel", concurrencyFlagNotPresent, "concurrency")
 	flag.Parse()
 
-	var URLs []string
-
-	if *concurrencyPtr != concurrencyDefaultValue {
-		URLs = os.Args[3:]
-	} else {
+	if *concurrencyPtr == concurrencyFlagNotPresent {
 		URLs = os.Args[1:]
+		concurrency = concurrencyDefaultValue
+	} else {
+		URLs = os.Args[3:]
+		concurrency = *concurrencyPtr
+	}
+
+	if len(URLs) == 0 {
+		return
 	}
 
 	requestGateway := gateways.NewGETRequest()
 	getHashResponsesUseCase := usecases.NewGetHashResponse(requestGateway)
 
-	for _, URL := range URLs {
-		resp, err := getHashResponsesUseCase.Execute(URL)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Printf("%s %s", URL, resp)
+	if concurrency == 1 {
+		runSequentially(URLs, getHashResponsesUseCase)
+		return
 	}
+
+	if concurrency > len(URLs) {
+		concurrency = len(URLs)
+	}
+
+	if concurrency > concurrencyMaxValue {
+		concurrency = concurrencyMaxValue
+	}
+
+	var (
+		ch = make(chan int, concurrencyMaxValue)
+		wg sync.WaitGroup
+	)
+
+	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
+		go func() {
+			for {
+				a, ok := <-ch
+				if !ok {
+					wg.Done()
+					return
+				}
+
+				makeRequests(URLs[a], getHashResponsesUseCase)
+			}
+		}()
+	}
+
+	for i := 0; i < len(URLs); i++ {
+		ch <- i
+	}
+
+	close(ch)
+	wg.Wait()
+}
+
+func runSequentially(URLs []string, getHashResponsesUseCase usecases.GetHashResponse) {
+	for i := 0; i < len(URLs); i++ {
+		makeRequests(URLs[i], getHashResponsesUseCase)
+	}
+}
+
+func makeRequests(URL string, getHashResponsesUseCase usecases.GetHashResponse) {
+	resp, err := getHashResponsesUseCase.Execute(URL)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(URL, resp)
 }
